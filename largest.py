a41839113cb2e7d2db0dc74bbede530861a18927
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-06-07 13:50:28 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -4,8 +4,10 @@ r=0
 num=input().split()
 for i in num:
   if r<a:
-    s1.append(i)
+    s1.append(int(i))
     r+=1
 s1.sort(reverse=True)
+a1=0
 for i in s1:
-  print(i,end="")
+  a1=(a1*10)+i
+print(a1)
