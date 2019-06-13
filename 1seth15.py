455fa229069bbd2ab17cba2c52e60ec29e006564
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-06-13 22:47:56 +0530

    Update and rename seth15.py to 1seth15.py

--- 1seth15.py
+++ 1seth15.py
@@ -7,17 +7,20 @@ for i in num:
   s1.append(int(i))
 c=0
 c1=0
-for i in range(0,n):
+for i in range(0,n-1):
   c=0
   co=i+1
   for j in range(i+1,n):
-    if s1[i]>s1[j] or i==j:
+    if s1[i]>s1[j]:
       c+=1
   if c==(n-co):
     s3.append(s1[i])
     c1+=1
 for i in range(c1):
   print(s3[i],end=" ")
+print(s1[n-1],end="")
+s3.append(s1[n-1])
 s3.sort()
 print('\n',end="")
-print(s3[c1-1])
+print(s3[c1])
+
